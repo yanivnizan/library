1100364c57943c8a71431d0bf37076f07ea54395
refaelos <refaeldakar@gmail.com>
2014-11-15 18:14:36 +0200

    removed unneeded setting for gappsdomain

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -56,8 +56,7 @@ var all = {
   google: {
     clientID:     process.env.GOOGLE_ID || 'id',
     clientSecret: process.env.GOOGLE_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback',
-    gAppsDomain: process.env.GAPPS_DOMAIN || ''
+    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
   },
 
   mixpanel: {
